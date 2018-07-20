65796239b13b2add3955db04d2b138c60ad606e0
zhuqifei <zhu_qifei@yahoo.com.sg>
2018-07-19 23:15:23 -0400

    Brent solver

--- solver/BrentSolver.py
+++ solver/BrentSolver.py
@@ -1,7 +1,11 @@
 from solver.Solver import Solver
 
+from scipy.optimize import optimize
+
 
 class BSSolver(Solver):
 
-    def solve(self):
-        return NotImplemented
+    def solve(self, f, target):
+        def f2(x):
+            return abs(f(x) - target)
+        return optimize.brent(f2)
